426928e1663b391bbc322b8bccfd8eeb28509c2b
泪莫倾城 <774645722@qq.com>
2022-12-04 20:42:19 +0800

    变更示例的版本信息

--- tools/demo/config.js
+++ tools/demo/config.js
@@ -1,5 +1,5 @@
-export const Version = 'v0.4.2';         // 当前版本
-export const UpdateDate = '2022-10-16'; // 更新时间
+export const Version = 'v1.1.0';        // 当前版本
+export const UpdateDate = '2022-12-04'; // 更新时间
 export const AppName = 'Mind UI Weapp'; // 组件库名称
 export const DocSite = 'https://mind-ui-weapp.ixook.com';   // 文档地址
 export const Logo = 'https://note-file.ixook.com/FkwInL0tWpqDeRNtYHMfmaHlioTq'; // Logo
